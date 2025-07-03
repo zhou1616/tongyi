e8c46e23eb384be8ced74f91ecda6b64285ab40f
zhou1616 <1678665048@qq.com>
2025-07-03 22:44:41 +0800

    ExpandablePanel

--- src/components/ExpandablePanel.vue
+++ src/components/ExpandablePanel.vue
@@ -5,15 +5,64 @@
 <template>
 <div>
   <div class="expand">
-    <div class="expand-1">
+    <div class="expand-1 ">
       <div class="expand-2">
-        <div class="side">
-          <button type="button" class="ui-button"></button>
+        <div class="headerWrapper">
+          <button type="button" class="ui-button">
+            <span role="img" class="icon">
+              <svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">
+                <use xlink:href="#tongyi-jia-plus-line"></use>
+              </svg>
+            </span>
+            新建对话
+          </button>
+          <div class="historySearch">
+            <span role="img" class="searchIcon">
+              <svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">
+                <use xlink:href="#icon-sousuo_default"></use>
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div class="sessionListWrapper">
+          <div class="sessionList"></div>
+        </div>
+        <div class="footer">
+          <div class="tip1">登录领权益</div>
+          <div class="tip2">领音视频时长、全文翻译数、存储空间等多项权益</div>
+          <button type="button" class="ui-button-2">立即登录</button>
+          <div class="downloadbox">
+            <div class="downloadBtn">
+              <span role="img" class="anticon">
+                <svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">
+                  <use xlink:href="#extend-mobilePhone-line"></use>
+                </svg>
+              </span>
+              下载手机应用
+            </div>
+            <div class="downloadBtn">
+              <span role="img" class="anticon">
+                <svg width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class="">
+                  <use xlink:href="#extend-plugin-line"></use>
+                </svg>
+              </span>
+              安装浏览器插件
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <span class="sc sc-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none"  width="5.997314453125" height="28.001953125" viewBox="0 0 5.997314453125 28.001953125">
+        <g>
+          <g>
+          <path d="M1.76479,1.83999484375L0.0229461,13.55206484375Q-0.0106777,13.77816484375,0.00468256,14.00546484375Q-0.00940751,14.22826484375,0.0235386,14.44976484375L1.76538,26.16176484375Q1.78073,26.26496484375,1.80613,26.36626484375Q1.83154,26.46766484375,1.86676,26.56616484375Q1.90198,26.66466484375,1.94667,26.75946484375Q1.99137,26.85416484375,2.04511,26.94426484375Q2.09885,27.03426484375,2.16112,27.11886484375Q2.22339,27.20336484375,2.29359,27.28156484375Q2.36379,27.35966484375,2.44124,27.43076484375Q2.51869,27.50176484375,2.60264,27.56506484375Q2.6866,27.62826484375,2.77625,27.68316484375Q2.86591,27.73796484375,2.96039,27.78386484375Q3.05488,27.82976484375,3.1533,27.86626484375Q3.25171,27.90286484375,3.3531,27.92966484375Q3.45449,27.95646484375,3.55788,27.97326484375Q3.66127,27.99006484375,3.76566,27.99676484375Q3.87005,28.00336484375,3.97445,27.99986484375Q4.07884,27.99626484375,4.18223,27.98256484375Q4.38946,27.95506484375,4.58672,27.88766484375Q4.78398,27.82036484375,4.96367,27.71576484375Q5.14337,27.61126484375,5.29861,27.47346484375Q5.45384,27.33566484375,5.57866,27.17006484375Q5.70352,27.00426484375,5.79313,26.81696484375Q5.88274,26.62966484375,5.93366,26.42796484375Q5.98459,26.22626484375,5.99486,26.01796484375Q6.00513,25.80966484375,5.97436,25.60276484375L4.2486,13.99886484375L5.97378,2.39897484375Q6.00455,2.19207484375,5.99427,1.98378484375Q5.984,1.77549484375,5.93308,1.57381484375Q5.88215,1.37214484375,5.79254,1.18482484375Q5.70293,0.99751484375,5.57807,0.83176884375Q5.45325,0.66608384375,5.29802,0.52831484375Q5.14278,0.39054584375,4.96308,0.28598684375Q4.78339,0.18142684375,4.58613,0.11409284375Q4.38888,0.04675804375,4.18164,0.01923584375Q4.07825,0.00550537375,3.97386,0.00195430175Q3.86947,-0.0015967662500000002,3.76507,0.005065743750000001Q3.66068,0.01172824375,3.55729,0.02854014375Q3.4539,0.04535214375,3.35251,0.07215154375Q3.25112,0.09895094375,3.15271,0.13547984375Q3.05429,0.17200884375,2.95981,0.21791484375Q2.86532,0.26382184375,2.77566,0.31866384375Q2.68601,0.37350584375,2.60205,0.43675484375Q2.5181,0.5000038437500001,2.44065,0.57105184375Q2.3632,0.64209984375,2.293,0.72026084375Q2.2228,0.79842284375,2.16053,0.88294584375Q2.09826,0.96746784375,2.04452,1.05753484375Q1.99078,1.14760484375,1.94608,1.24235484375Q1.90139,1.33710484375,1.86617,1.43562484375Q1.83095,1.53414484375,1.80554,1.63547484375Q1.78014,1.73681484375,1.76479,1.83999484375Z" fill-rule="evenodd" fill-opacity="1" data-spm-anchor-id="5176.28103460.0.i7.2f2e1db8pcvg4R"></path>
+        </g>
+        </g>
+      </svg>
+    </span>
   </div>
-</div>
+  </div>
 </template>
 
 <style scoped>
@@ -66,5 +115,153 @@
   opacity: 1;
   transition: all .3s;
   width: 168px;
+}
+.headerWrapper{
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin: 0 24px 28px;
+}
+.icon{
+  font-size: 20px;
+  margin-right: 8px;
+  color: #fff !important;
+  cursor: not-allowed !important;
+  display: inline-flex;
+  align-items: center;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+}
+.historySearch{
+  align-items: center;
+  border: 1px solid #e8eaf2;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: row;
+  height: 36px;
+  justify-content: center;
+  position: relative;
+  transition: all .3s;
+  width: 36px;
+  cursor: not-allowed !important;
+}
+.searchIcon{
+  color: #2c2c36;
+  font-size: 20px;
+  display: inline-flex;
+  align-items: center;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+}
+.sessionListWrapper{
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  overflow: auto;
+}
+.sessionList{
+  flex: 1;
+  overflow-y: auto;
+  padding: 0 24px;
+}
+.footer{
+  border-top: 1px var(#e8eaf2);
+  margin: 0 24px;
+  padding-top: 12px;
+  position: relative;
+
+}
+.tip1{
+  color: #2c2c36;
+  font-size: 16px;
+  font-weight: 500;
+  padding-bottom: 4px;
+  padding-top: 12px;
+  text-align: center;
+}
+.tip2{
+  color: #585a73;
+  font-size: 14px;
+  padding: 8px 10px 32px;
+  text-align: center;
+}
+.ui-button-2{
+  background: var(63deg, #615ced);
+  border: none;
+  color: var(#fff);
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  align-items: center;
+  background-image: none;
+  border-radius: 21px;
+  box-shadow: none;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-flex;
+  font-size: 16px;
+  height: 32px;
+  justify-content: center;
+  outline: none;
+  padding: 4px 15px;
+  position: relative;
+  touch-action: manipulation;
+  transition: all .2s cubic-bezier(.645,.045,.355,1);
+  user-select: none;
+  white-space: nowrap;
+}
+.downloadbox{
+  border-top: 1px solid #e8eaf2;
+  padding-top: 16px;
+}
+.downloadBtn{
+  align-items: center;
+  background-color: #fff;
+  border: 1px solid #e8eaf2;
+  border-radius: 32px;
+  color: #2c2c36;
+  cursor: pointer;
+  display: flex;
+  font-size: 14px;
+  height: 36px;
+  justify-content: center;
+  transition: all .4s ease;
+  width: 100%;
+}
+.anticon{
+  display: inline-flex;
+  align-items: center;
+  color: inherit;
+  font-style: normal;
+  line-height: 0;
+  text-align: center;
+  text-transform: none;
+  vertical-align: -0.125em;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+}
+.sc{
+  transform: translate(0px, -22px) rotate(180deg);
+}
+.sc-1{
+  position: absolute;
+  right: -22px;
+  top: 50%;
+  transform: translate(0px, -22px) rotateY(0deg);
+  width: 22px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  z-index: 11;
 }
 </style>
